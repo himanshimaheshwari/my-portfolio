6ddc10cf9c45496c0dee4d182f8ed893a3397eee
himanshimaheshwari <maheshwarihimanshi18@gmail.com>
2025-07-09 15:30:36 +0530

    Initial portfolio upload

---
+++ src/data/constants.js
@@ -0,0 +1,317 @@
+export const Bio = {
+  name: "Himanshi Maheshwari",
+  roles: [
+    "Data Scientist",
+    "Data Analyst",
+    "Research Analyst",
+    "Programmer",
+  ],
+  description:
+    "I am a self-driven and curious individual passionate about data, AI, and solving real-world problems through technology. With a strong foundation in machine learning and hands-on experience in impactful projects, I’m always eager to learn, adapt, and contribute meaningfully to innovative work.",
+  github: "https://github.com/himanshimaheshwari",
+  resume:
+    "https://drive.google.com/file/d/149OReqBf-n21nlMAQ9fIflTYUSNPQFeG/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/himanshi-maheshwari-1690a625b/",
+  insta: "https://www.instagram.com/hiimanshii_18/",
+};
+
+export const skills = [
+  {
+    title: "Data Science & Machine Learning",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/800px-Python-logo-notext.svg.png",
+      },
+      {
+        name: "Pandas",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlmPgS_sE-w6fNtTtXRG9Mkhjzh6DTks0Aiw&s",
+      },
+      {
+        name: "NumPy",
+        image:
+          "https://www.pythontutorial.net/wp-content/uploads/2022/08/numpy-tutorial.svg",
+      },
+      {
+        name: "TensorFlow",
+        image: "https://avatars.githubusercontent.com/u/15658638?s=280&v=4",
+      },
+      {
+        name: "PyTorch",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlYzcR0Bdk8Qr8DywxsGKJ38IUc__j6zViPQ&s",
+      },
+      {
+        name: "Scikit-Learn",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/1200px-Scikit_learn_logo_small.svg.png",
+      },
+      {
+        name: "Tranformers",
+        image:
+          "https://miro.medium.com/v2/resize:fit:631/0*SUe9oh9zYtGBbnlm.png",
+      },
+      {
+        name: "Time Series",
+        image:
+          "https://www.tigerdata.com/_next/image?url=https%3A%2F%2Ftimescale.ghost.io%2Fblog%2Fcontent%2Fimages%2F2023%2F04%2Ftime-series-analysis_img3.png&w=3840&q=100",
+      },
+    ],
+  },
+  {
+    title: "Data Handling & Big Data",
+    skills: [
+      {
+        name: "PySpark",
+        image: "https://saturncloud.io/images/glossary/pyshark.webp",
+      },
+      {
+        name: "Hadoop",
+        image:
+          "https://miro.medium.com/v2/resize:fit:1400/1*aFPoPQSaO4DFD7ezZC8WrQ.png",
+      },
+      {
+        name: "Cloudera",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGbvoF0ah0TuAF5joJmN5CVZrIc28NrJt6qA&s",
+      },
+      {
+        name: "Apache Pig",
+        image:
+          "https://upload.wikimedia.org/wikipedia/en/thumb/b/bc/Apache_Pig_Logo.svg/1200px-Apache_Pig_Logo.svg.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Jupyter Notebook",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1200px-Jupyter_logo.svg.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Google Colab",
+        image: "https://algotrading101.com/learn/wp-content/uploads/2021/05/Google-Colab-Guide-e1620759490851.jpg",
+      },
+    ],
+  },
+  {
+    title: "Visualization & Reporting",
+    skills: [
+      {
+        name: "Matplotlib",
+        image:
+          "https://miro.medium.com/v2/resize:fit:1200/1*Kzb1TcQsx4F12t-qsU7knA.png",
+      },
+      {
+        name: "Seaborn",
+        image:
+          "https://avatars.githubusercontent.com/u/22799945?s=280&v=4",
+      },
+      {
+        name: "Plotly",
+        image:
+          "https://store-images.s-microsoft.com/image/apps.36868.bfb0e2ee-be9e-4c73-807f-e0a7b805b1be.712aff5d-5800-47e0-97be-58d17ada3fb8.a46845e6-ce94-44cf-892b-54637c6fcf06",
+      },
+      {
+        name: "Power BI",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW1erUjlH647ky8bzEG7utwSHZKxFKx9-ESEhf2pL1bkp22FSqmBJD9rTBIxAcSqw_B2o&usqp=CAU",
+      },
+      {
+        name: "Tableau",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaeDoXpY4WQwBG6ZyCYDVzDeuWYMQEeDlNFiKSAPGM8WgammeuxnQe3iHfPU4tffgxODs&usqp=CAU",
+      },
+      {
+        name: "Folium",
+        image:
+          "https://python-visualization.github.io/folium/latest/_images/folium_logo.png",
+      },
+      {
+        name: "Excel",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1ftoNCG6adcKvQkwZqmv9dLhIcVrMavS616bGKifPhS4tlVg6Wkwh6OBzTufs940PiHA&usqp=CAU",
+      },
+    ],
+  },
+  {
+    title: "Tools & Development",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXp1dj5t9mzbsWoKNWSXW9UKkgyF45KAjuRA&s",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://nordicapis.com/wp-content/uploads/How-to-Create-an-API-Using-The-Flask-Framework.png",
+      },
+      {
+        name: "Streamlit",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTfbVS_tngm79fnju-ZfBTcADvKP9bjUbN1mZOWXX2E99YQ3TxKDz4jGVgZwz9Mv0amHw&usqp=CAU",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Tkinter",
+        image:
+          "https://miro.medium.com/v2/resize:fit:680/1*AIlWq29GeP1eny3wA7aMgA.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/D560BAQHmu41EmpRp2w/company-logo_200_200/company-logo_200_200/0/1736681247403/rym_grenergy_logo?e=2147483647&v=beta&t=NBpqU5mKNR7V9X7A4YVysAVjD8GOS0TCbGhBPqv2twE",
+    role: "Data Science Intern",
+    company: "RYM Grenergy Solution pvt. ltd",
+    date: "June 2024 - November 2024",
+    desc: "Worked on AI-powered optimization systems to improve efficiency and automation in real-world infrastructure scenarios.",
+    skills: [
+      "Python",
+      "Machine Learning",
+      "Tkinter",
+      "CustomTkinter",
+      "XGBoost",
+      "Pandas",
+      "NumPy",
+      "Scikit-learn",
+      "Matplotlib",
+      "Seaborn",
+      "IoT Concepts",
+      "Optimization Algorithms",
+      "Data Visualization",
+      "GUI Development"
+    ],
+    doc: "https://drive.google.com/file/d/1XfnJnjgbEW0Gp0IBErvcmKkBmZFDHN6f/view?usp=sharing",
+  },
+  {
+    id: 0,
+    img: "https://swissvogue.in/cdn/shop/files/logo_57d4ec71-94e0-49aa-a25b-4f46a11b5d86.png?v=1751136823&width=600",
+    role: "Data Analyst Intern",
+    company: "SwissVogue",
+    date: "June 2025 - Present",
+    desc: "Worked on enhancing SwissVogue’s e-commerce platform by designing data-driven product insights, building category structures, and refining user interface flows",
+    skills: [
+      "Python",
+      "Pandas",
+      "NumPy",
+      "Scikit-learn",
+      "Matplotlib",
+      "Seaborn",
+      "Excel",
+      "Linear Regression",
+      "Classification",
+      "Data Visualization",
+      "UI/UX Structuring",
+      "E-commerce Domain Knowledge",
+    ],
+    doc: "https://drive.google.com/file/d/1n6OG6GqRQAIyUltbRcMGWD15b2eXM477/view?usp=sharing",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.mycampusreview.com/assest/front/images/fb/bennut.jpg",
+    school: "Bennett University, Greater Noida",
+    date: "Aug 2022 - Aug 2026",
+    grade: "8.45 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering with a specialization in Data Science from JECRC University, holding a CGPA of 8.45 after 4 semesters. My coursework includes subjects like Data Structures, Algorithms, Object-Oriented Programming, DBMS, Operating Systems, and Computer Networks, alongside advanced data science concepts. I am passionate about applying data-driven techniques to solve real-world problems and continue to strengthen my skills through practical projects and internships.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Trendlens",
+    description:
+      "TrendLens is a smart desktop GUI application that enables detailed sales forecasting, seasonal pattern detection, and data-driven recommendations for popular Indian e-commerce platforms: Meesho, Flipkart, and Amazon. Built using Python, Tkinter, and powerful AI models, TrendLens empowers businesses to make data-backed decisions using time series analysis and machine learning.",
+    image:
+      "https://miro.medium.com/v2/resize:fit:729/1*jKFm2Dy7KTfsWgtgIpmSjg.png",
+    tags: ["Python", "Tkinter", "Plotly", "LSTM", "ARIMA", "SARIMA"],
+    github: "https://github.com/rishavchanda/https://github.com/himanshimaheshwari/TrendLens-Intelligent-E-Commerce-Sales-Forecasting-and-Recommendation-System",
+  },
+  {
+    id: 1,
+    title: "Tableau Dashboard",
+    description:
+      "This project presents an interactive sales dashboard built using Tableau, designed to analyze sales trends, product performance, regional distribution, and customer segmentation. The dashboard provides data-driven insights to help businesses optimize their strategies and decision-making.",
+    image:
+      "https://cdnl.tblsft.com/sites/default/files/blog/lovelytics_hr.png",
+    tags: [
+      "Python",
+      "Tableau",
+      "Data Cleaning",
+    ],
+    github: "https://github.com/himanshimaheshwari/Sales-Dashboard-Tableau-Python?tab=readme-ov-file",
+  },
+  {
+    id: 2,
+    title: "Scheme Research Tool",
+    description:
+      "Engineered a research tool for government schemes using NLP and real-time API integration, processing 100K+ scheme records with BeautifulSoup, Requests, and OpenAI’s GPT models for precise text extraction and summarization.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9NKps8MN_AG7emIKAzgo6yLwWLWzqqpyijw&s",
+    tags: ["Python", "Keras", "TensorFlow", "Tkinter", "BeautifulSoup"],
+    github: "https://github.com/himanshimaheshwari/Scheme-Research-Tool",
+  },
+  {
+    id: 3,
+    title: "MPIN Validator",
+    description:
+      "This project implements a complete MPIN (Mobile Personal Identification Number) Validator in Python. It detects weak 4-digit and 6-digit MPINs based on patterns, demographics (like DOB, spouse DOB, anniversary), and randomness (entropy). The logic is built step-by-step through multiple modules, all of which are included in this repository.",
+    image:
+      "https://www.kotak.com/content/dam/Kotak/product_card_images/mpin-meaning-t.jpg",
+    tags: ["Data Science", "Palindrome", "Python", "Data Analysis"],
+    github: "https://github.com/himanshimaheshwari/MPIN-VALIDATOR",
+  },
+  {
+    id: 10,
+    title: "Stock Price Analysis",
+    description:
+      "A time series analysis project using a dataset with a time component, specifically historical stock prices. The objective is to uncover patterns, trends, and insights from the temporal data, enabling a better understanding of stock price movements over time.",
+    image:
+      "https://www.mdpi.com/mathematics/mathematics-11-01130/article_deploy/html/images/mathematics-11-01130-g001.png",
+    tags: ["Python", "Stock Price", "EDA"],
+    github: "https://github.com/himanshimaheshwari/stock-price-analysis",
+  },
+  {
+    id: 4,
+    title: "EV Sales Analysis",
+    description:
+      "Apache Spark is a fast, distributed, and general-purpose cluster computing framework. It is designed for big data processing with in-memory computation capabilities, making it much faster than traditional disk-based systems like Hadoop's MapReduce.",
+    image:
+      "https://canalys-prod-public.s3.eu-west-1.amazonaws.com/cosi/campaign/3770/_37i4bj0qx~l2Ik7VGgBf_cubNBC66p4.png",
+    tags: ["Hadoop", "Apache Spark", "Python", "Big Data"],
+    github: "https://github.com/himanshimaheshwari/Apache-Spark-based-analysis-as-action-and-transformation-on-electric-vehicles-sales-dataset.",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
